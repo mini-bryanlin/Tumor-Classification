9e40fd86b834e404ecffa43fefb2fa8017eae849
Bryan_Lin <bryanlin316@gmail.com>
2025-03-01 23:18:43 -0500

    mysql init

---
+++ DataBase/load_data.session.sql
@@ -0,0 +1,8 @@
+SHOW databases
+
+-- @block
+create table BC_DATA (
+    id int
+)
+-- @block
+drop table BC_DATA
---
+++ DataBase/sql_storing.py
@@ -0,0 +1,50 @@
+import mysql.connector
+import pandas as pd
+import os
+import csv
+from sqlalchemy import create_engine
+
+# Expand the tilde to your actual home directory path
+file_path = os.path.expanduser("~/password/sql.txt")
+
+def get_header(file_path):
+    data = pd.read_csv(file_path)
+    column_headers = list(data.columns.values)
+    return column_headers
+
+headers = get_header("~/Tumor-Classification/breast-cancer.csv")
+file = open(file_path,'r')
+password = file.readline()
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = password,
+    database = "neuralnetwork"
+
+)
+bc_file = os.path.expanduser("~/Tumor-Classification/breast-cancer.csv")
+mycursor = mydb.cursor()
+# mycursor.execute("DROP TABLE BC_DATA")
+def load_csv_to_mysql(csv_file_path, table_name, if_exists='replace', 
+                     host='localhost', user='root', password='', database=''):
+   
+    # Read the CSV file
+    df = pd.read_csv(csv_file_path)
+    
+    # Create connection string
+    connection_str = f"mysql+mysqlconnector://{user}:{password}@{host}/{database}"
+    
+    # Create SQLAlchemy engine
+    engine = create_engine(connection_str)
+    
+    # Load the data into the table
+    df.to_sql(table_name, engine, if_exists=if_exists, index=False)
+    
+    print(f"Data from {csv_file_path} successfully loaded to {table_name} table in MySQL database {database}")
+# load_csv_to_mysql(file_path,"BC_DATA",password = password, database= "neuralnetwork")
+
+# mycursor.execute("drop table BC_DATA")
+mycursor.execute("create table BC_DATA (id int(255))")
+for header in headers:
+    if header != "id":
+        mycursor.execute(f"alter table BC_DATA add {header} FLOAT ")
