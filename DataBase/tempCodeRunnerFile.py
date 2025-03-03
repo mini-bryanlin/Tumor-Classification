c3f0ed9ecda0859d504e07857ea57573e1cd30a2
mini-bryanlin <bryanlin316@gmail.com>
2025-03-02 22:24:07 -0500

    ntn

--- DataBase/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-mycursor.execute("create table BC_DATA (id int(255))")
-# for header in headers:
-#     if header != "id":
-#         mycursor.execute(f"alter table BC_DATA add {header} FLOAT ")
--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-mycursor.execute("create table BC_DATA (id int auto_increment primary key) ")
-mycursor.execute("drop table BC_DATA")
-mycursor.execute("create table BC_DATA (id int(255))")
-for header in headers:
-    if header != "id":
-        mycursor.execute(f"alter table BC_DATA add {header} FLOAT ")
